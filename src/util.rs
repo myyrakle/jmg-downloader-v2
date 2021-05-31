0d02cbae8a446e24f554f5290d080a01425ea5db
myyrakle <sssang97@naver.com>
2021-06-01 01:12:37 +0900

    주석 추가

--- src/util.rs
+++ src/util.rs
@@ -9,6 +9,7 @@ use url::Url;
 
 use crate::constants::*;
 
+// 다음 요소가 있는지 체크
 pub fn has_next(tab: &Tab) -> Result<bool, Box<dyn std::error::Error>> {
     let class = tab.find_elements(NEXT_BUTTON_SELECTOR)?[1]
         .call_js_fn("function() { return this; }", false)?;
@@ -16,11 +17,13 @@ pub fn has_next(tab: &Tab) -> Result<bool, Box<dyn std::error::Error>> {
     Ok(class.description != Some("span.disabled".into()))
 }
 
+// 다음으로 이동
 pub fn do_next(tab: &Tab) -> Result<(), Box<dyn std::error::Error>> {
     tab.wait_for_elements(NEXT_BUTTON_SELECTOR)?[1].click()?;
     Ok(())
 }
 
+// 요소의 텍스트 파싱
 pub fn get_content(element: Vec<Element>) -> Option<String> {
     let texts: Option<Vec<String>> = element
         .into_iter()
@@ -37,6 +40,7 @@ pub fn get_content(element: Vec<Element>) -> Option<String> {
     Some(texts?.join("\n").to_string())
 }
 
+// 파일에 텍스트 이어쓰기
 pub fn file_write(filename: &str, content: String) -> Result<(), Box<dyn std::error::Error>> {
     if Path::new(&filename).exists() == false {
         File::create(&filename)?;
@@ -49,6 +53,7 @@ pub fn file_write(filename: &str, content: String) -> Result<(), Box<dyn std::er
     Ok(())
 }
 
+// 다운로드 로직
 pub fn download(tab: &Tab) -> Result<(), Box<dyn std::error::Error>> {
     let query: HashMap<_, _> = Url::parse(&tab.get_url())?
         .query_pairs()
